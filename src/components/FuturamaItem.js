b6b2d57226c3f6875f1ce907ede98e66763164ad
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-28 17:37:07 -0400

    pass futuramaItem test

--- src/components/FuturamaItem.js
+++ src/components/FuturamaItem.js
@@ -3,11 +3,12 @@ import Component from './Component.js';
 class FuturamaItem extends Component {
 
     renderTemplate() {
+        const quote = this.props.quote;
         return /*html*/ `
         <li>
-            <h2> "Dr Zoidberg",</h2>
-            <img src="https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904014/Dr_John_Zoidberg.png">
-            <blockquote>"Bender, you look awful. I recommend makeup."</blockquote>
+            <h2>${quote.character}</h2>
+            <img src="${quote.image}">
+            <blockquote>${quote.quote}</blockquote>
         </li>
         `;
     }
--- test/futurama-item.test.js
+++ test/futurama-item.test.js
@@ -17,8 +17,8 @@ test('renders template from data', assert => {
     //assert
     assert.htmlEqual(rendered, /*html*/`
         <li>
-            <h2> "Dr Zoidberg",</h2>
+            <h2>Dr Zoidberg</h2>
             <img src="https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904014/Dr_John_Zoidberg.png">
-            <blockquote>"Bender, you look awful. I recommend makeup."</blockquote>
+            <blockquote>Bender, you look awful. I recommend makeup.</blockquote>
         </li>`);
 });
